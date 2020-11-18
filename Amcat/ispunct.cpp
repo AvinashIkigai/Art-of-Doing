238c00ce72f706586eed90395ff5729ca360c1b0
Avinash <avinash.ikigai@gmail.com>
2020-11-18 20:34:25 +0530

    removed

--- Amcat/ispunct.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	string s;
-	getline(cin, s);
-	for(int i=0;i<s.length();i++) //printing the string if its special symbol like @#^&* etc.
-		if(ispunct(s[i]))
-			cout << s[i];
-}
