297405700ab599f110dec0fac54a2375e5c0ecb0
Avinash <avinash.ikigai@gmail.com>
2020-12-22 11:08:38 +0530

    TicTacToe

--- BankApp.py
+++ BankApp.py
@@ -1,5 +1,3 @@
-
-
 def get_info():
     """Get User info to store in a dict"""
     print("\nWelcome to the Python First National Bank.")
@@ -8,35 +6,39 @@ def get_info():
     checking = int(input("How much money would you like to set up your checking account with: "))
 
     bank_account = {
-        "Name":name,
-        "Savings":savings,
-        "Ckecking":checking,
+        "Name": name,
+        "Savings": savings,
+        "Ckecking": checking,
     }
     return bank_account
+
 
 def make_deposite(bank_account, account, money):
     """Withdraw money from a specific type of account"""
     bank_account[account] += money
-    print("\nDeposited $"+str(money)+" into " +bank_account['Name']+ "'s " + account.lower() + " account.")
+    print("\nDeposited $" + str(money) + " into " + bank_account['Name'] + "'s " + account.lower() + " account.")
 
-def make_withdrawl(bank_account,account,money):
+
+def make_withdrawl(bank_account, account, money):
     """Withdraw money from a specific type of account"""
     if bank_account[account] - money >= 0:
         bank_account[account] -= money
-        print("\nWithdrew $"+str(money)+" from "+bank_account['Name']+" 's " + account.lower() + " account.")
+        print("\nWithdrew $" + str(money) + " from " + bank_account['Name'] + " 's " + account.lower() + " account.")
     else:
-        print("\nSorry, by withdrawing $"+str(money)+" you will have a negative balance.")
+        print("\nSorry, by withdrawing $" + str(money) + " you will have a negative balance.")
+
 
 def display_info(bank_account):
     """Display all key-value pairs in a given bank account"""
     print("\nCurrent Account Information")
     for key, value in bank_account.items():
         if key == 'Name':
-            print(key+": " +str(value))
+            print(key + ": " + str(value))
         else:
             print(key + ": $" + str(value))
 
-#The Main code
+
+# The Main code
 my_account = get_info()
 running = True
 while running:
@@ -47,9 +49,9 @@ while running:
 
     if account_type == "Savings" or account_type == "Checking":
         if choice == "Deposit":
-            make_deposite(my_account,account_type,amount)
+            make_deposite(my_account, account_type, amount)
         elif choice == "Withdrawl":
-            make_withdrawl(my_account,account_type,amount)
+            make_withdrawl(my_account, account_type, amount)
         else:
             print("\nI am sorry, We cannot do that for you today.")
     else:
